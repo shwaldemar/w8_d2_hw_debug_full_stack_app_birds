70ba4b2bfb04c22f5dc2103c96041edf1dd0cebc
Simon <shwaldemar@gmail.com>
2019-07-02 17:27:11 +0100

    server and router running and routes tested in insomnia front end started

---
+++ client/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <sightings-form />
+    <sightings-grid :sightings="sightings" />
+  </div>
+</template>
+
+<script>
+import SightingsForm from './components/SightingsForm';
+import SightingsGrid from './components/SightingsGrid';
+import { eventBus } from './main';
+
+export default {
+  name: 'app',
+  components: {
+    'sightings-grid': SightingsGrid,
+    'sightings-form': SightingsForm
+  },
+  data () {
+    return {
+      sightings: []
+    }
+  },
+  components: {
+    'sightings-form': SightingsForm,
+    'sightings-grid': SightingsGrid
+  },
+  mounted(){
+    this.fetchData();
+  },
+  methods: {
+    fetchData(){
+      fetch("http://localhost:3000/api/sightings")
+        .then(res => res.json())
+        .then(sightings => this.sightings = sightings);
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  background: url('./assets/birds-background.jpg') no-repeat;
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+}
+</style>
